b824deb6253e1774683358ee14853bab8053b5e0
zu-hwang <tropical.kitsch@gmail.com>
2020-07-01 21:00:17 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import App from 'src/pages';
 import store from 'src/redux/store';
 import GlobalStyles from 'src/util/GlobalStyles';
 
+// store.getState() 메서드를 통해 스토어의 모든 state에 접근할 수 있다.
+const allData = store.getState();
+console.log('allData', allData);
+const { todolist } = allData.todoReducer;
+const { modal } = allData.footerReducer;
+console.log('modal', modal, 'todolist', todolist);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
